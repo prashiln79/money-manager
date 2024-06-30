91eea18fd47d703b30800d421b9e56676a0afa5b
prashil <prashiln79@gmail.com>
2024-07-01 00:09:43 +0530

    init setup

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Money Manager ',
-  webDir: 'www'
-};
-
-export default config;
