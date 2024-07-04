c0eebdbcd4445c2241e63e7e3476854f1425c48a
prashil <prashiln79@gmail.com>
2024-07-04 23:09:11 +0530

    folder Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,23 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import { IconModule } from './icon.module';
+import { IconModule } from './util/icon.module';
 import { HttpClientModule } from '@angular/common/http';
+import { TotalBalanceComponent } from './component/total-balance/total-balance.component';
+import { CalendarViewComponent } from './component/calendar-view/calendar-view.component';
+import { TransactionListComponent } from './component/transaction-list/transaction-list.component';
+import { AddTransactionComponent } from './component/add-transaction/add-transaction.component';
+import { HeaderComponent } from './component/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TotalBalanceComponent,
+    CalendarViewComponent,
+    TransactionListComponent,
+    AddTransactionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/add-transaction/add-transaction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-transaction',
+  templateUrl: './add-transaction.component.html',
+  styleUrl: './add-transaction.component.scss'
+})
+export class AddTransactionComponent {
+
+}
