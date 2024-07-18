c007fcf36ebed204872f3f7f23ae746b11fc9a7f
prashil <prashiln79@gmail.com>
2024-07-19 00:20:38 +0530

    css changes added

--- src/app/component/transaction-list/add-transaction/transaction/transaction.component.ts
+++ src/app/component/transaction-list/add-transaction/transaction/transaction.component.ts
@@ -11,12 +11,15 @@ export class TransactionComponent {
 
   transactionForm: FormGroup;
   categories: string[] = ['Food', 'Travel', 'Entertainment', 'Health', 'Other'];
+  buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'];
 
 
-  constructor(private fb: FormBuilder,public dialogRef: MatDialogRef<TransactionComponent>) {
+  constructor(private fb: FormBuilder, public dialogRef: MatDialogRef<TransactionComponent>) {
     this.transactionForm = this.fb.group({
       name: ['', Validators.required],
       amount: ['', [Validators.required, Validators.min(0.01)]],
+      expression: [''],
+
       category: ['', Validators.required],
       date: ['', Validators.required],
       notes: ['']
@@ -31,6 +34,26 @@ export class TransactionComponent {
       console.log('Transaction Data:', this.transactionForm.value);
       // Here you can add the code to save the transaction data
     }
+  }
+
+  appendExpression(value: string): void {
+    if (value === '=') {
+      this.calculate();
+    } else {
+      this.transactionForm.get("amount")?.setValue(this.transactionForm.get("amount")?.value + value);
+    }
+  }
+
+  calculate(): void {
+    try {
+      this.transactionForm.get("amount")?.setValue(eval(this.transactionForm.get("amount")?.value).toString());
+    } catch (e) {
+      this.transactionForm.get('amount')?.setValue('Error');
+    }
+  }
+
+  clear(): void {
+    this.transactionForm.get('amount')?.setValue('');
   }
 
 }
