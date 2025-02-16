2e185ab9ec1797193c514a3e9f3f47a2e37051fc
prashil <prashiln79@gmail.com>
2025-02-16 14:49:34 +0530

    mobile screen checking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -109,12 +109,11 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 
     // Utility
     IconModule,
-     ServiceWorkerModule.register('ngsw-worker.js', {
-       enabled: !isDevMode(),
-       // Register the ServiceWorker as soon as the application is stable
-       // or after 30 seconds (whichever comes first).
-       registrationStrategy: 'registerWhenStable:30000'
-     }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerImmediately'
+    }),
+    
 
     // Service Worker
     // ServiceWorkerModule.register('ngsw-worker.js', {
