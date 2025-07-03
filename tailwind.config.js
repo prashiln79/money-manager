ff6003a47077917a9702125c89da55ab0e323e3b
prashil <prashiln79@gmail.com>
2025-07-03 19:28:08 +0530

    Css changes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,38 @@ module.exports = {
     extend: {
       colors:{
         primary: '#46777f', // primary color
-      }
+      },
+      screens: {
+        'xs': '475px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        '3xl': '1920px',
+        '4xl': '2560px',
+      },
+      maxWidth: {
+        '8xl': '88rem',
+        '9xl': '96rem',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1536px',
+        },
+      },
     },
   },
   plugins: [],
