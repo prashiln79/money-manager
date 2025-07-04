e201c6671271e4f6d3248e002afeeadac4dc59a5
prashil <prashiln79@gmail.com>
2025-07-04 20:32:33 +0530

    pwa changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,22 @@ module.exports = {
       colors:{
         primary: '#46777f', // primary color
       },
+      fontSize: {
+        // Mobile-optimized font sizes
+        'xs': ['14px', { lineHeight: '1.4' }], // Increased from 12px
+        'sm': ['16px', { lineHeight: '1.5' }], // Increased from 14px
+        'base': ['18px', { lineHeight: '1.6' }], // Increased from 16px
+        'lg': ['20px', { lineHeight: '1.6' }],
+        'xl': ['22px', { lineHeight: '1.5' }],
+        '2xl': ['24px', { lineHeight: '1.4' }],
+        '3xl': ['28px', { lineHeight: '1.3' }],
+        '4xl': ['32px', { lineHeight: '1.2' }],
+        '5xl': ['36px', { lineHeight: '1.1' }],
+        '6xl': ['40px', { lineHeight: '1.1' }],
+        '7xl': ['48px', { lineHeight: '1' }],
+        '8xl': ['56px', { lineHeight: '1' }],
+        '9xl': ['64px', { lineHeight: '1' }],
+      },
       screens: {
         'xs': '475px',
         'sm': '640px',
