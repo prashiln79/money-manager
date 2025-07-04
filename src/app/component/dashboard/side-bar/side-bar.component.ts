02e3e71f767a57b3a522231d3e9fea9d42344fa7
prashil <prashiln79@gmail.com>
2025-07-04 10:25:51 +0530

    side bar changes added

--- src/app/component/dashboard/side-bar/side-bar.component.ts
+++ src/app/component/dashboard/side-bar/side-bar.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ThemeSwitchingService } from '../../../util/service/theme-switching.service';
 import { ThemeType } from 'src/app/util/models/theme.model';
 import { Auth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { LanguageSwitcherComponent } from '../../../util/components/language-switcher/language-switcher.component';
+import { ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'side-bar',
@@ -11,13 +13,20 @@ import { LanguageSwitcherComponent } from '../../../util/components/language-swi
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent {
-  
+  @ViewChild('drawer') drawer!: MatDrawer;
 
   constructor(
     private _themeSwitchingService: ThemeSwitchingService,
     private auth: Auth,
     private router: Router
   ){}
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Escape' && this.drawer && this.drawer.opened) {
+      this.drawer.close();
+    }
+  }
 
   public changeTheme(theme:ThemeType,drawer:any){
     this._themeSwitchingService.currentTheme.next(theme);
