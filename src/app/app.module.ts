6182da65e47e03a785a3b7e7b7511ffd62bd9b45
prashil <prashiln79@gmail.com>
2025-07-16 16:53:10 +0530

    issue fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, isDevMode } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -234,6 +234,7 @@ import { RouterModule } from '@angular/router';
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([securityInterceptor])),
     Papa,
+    provideClientHydration(),
 
     // Firebase Initialization
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
